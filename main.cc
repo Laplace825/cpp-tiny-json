5be806f06da0a3895145958a7202d7d5cec6bf59
Laplace825 <laplace_yongle_he@163.com>
2024-07-15 00:01:58 +0800

    make args vector in a concise way

--- main.cc
+++ main.cc
@@ -1,21 +1,21 @@
 /**
  * @author: Laplace825
  * @date: 2024-06-28 10:23:40
- * @lastmod: 2024-06-30T20:53:14
+ * @lastmod: 2024-07-14T23:58:24
  * @description: the main file to deal command line args
  * @filePath: /tiny-json/main.cc
  * @lastEditor: Laplace825
  * @ MIT license
  */
 
-#include <format>
-#include <iostream>
-#include <string_view>
-
 #include "tjson.hpp"
 #include "tjson/tjfile.hpp"
 #include "tjson/tjprint.hpp"
 #include "tjson/tjsonParser.hpp"
+
+#include <format>
+#include <iostream>
+#include <string_view>
 
 using namespace lap::tjson;
 
@@ -34,7 +34,7 @@ void help_msg()
                  "[example]:\n"
                  "tjson -r ./file.json\n -s ./file_store.json\n";
     std::cout << std::format("Now in path \033[1;32m{}\033[0m\n",
-                             std::filesystem::current_path().c_str());
+        std::filesystem::current_path().c_str());
 }
 
 void what_to_do(std::string_view command, std::string_view readStoreOrFind = "")
@@ -85,14 +85,10 @@ auto main(int argc, char* argv[]) -> signed
         help_msg();
         return 0;
     }
-    std::vector< std::string_view > args(argc); // store all the args
-    for (auto& arg : args)
-    {
-        arg = argv[0];
-        ++argv;
-    }
+    std::vector< std::string_view > args(
+        argv + 1, argv + argc); // store all the args
 
-    for (size_t i = 1; i < args.size();)
+    for (size_t i = 0; i < args.size();)
     {
         if (args[i] == "-h" || args[i] == "--help")
         {
